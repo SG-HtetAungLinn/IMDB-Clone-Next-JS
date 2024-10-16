bead792479922bc67a445245c12b7369cfe26d18
SG-Htet Aung Linn <softguide.htetaunglinn@gmail.com>
2024-10-16 15:48:06 +0630

    Create the about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div className="max-w-6xl mx-auto p-3 space-y-4">
+            <h1 className="text-2xl font-medium text-amber-600">About</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Obcaecati beatae repudiandae omnis reiciendis eius at
+                repellendus, molestias non hic, veritatis id doloribus rem
+                officiis dolore ipsam cumque autem facere soluta.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptas, illum numquam, quos explicabo odio a ex eius animi
+                consequatur in inventore earum fuga iure porro vel quaerat
+                molestias. Nemo, nobis.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+                consequuntur commodi alias totam eveniet eos. Reiciendis numquam
+                ut mollitia, architecto nulla illo dolor suscipit doloribus
+                dicta deleniti nostrum. Recusandae vel nisi hic sequi architecto
+                veritatis ipsa quisquam laborum iusto expedita, quas aliquam nam
+                ab ex rerum amet a atque tempore minima ut fuga aperiam.
+                Voluptatem, quibusdam aperiam sed ratione ab culpa
+                exercitationem. Debitis maiores asperiores ipsa, non at et
+                labore placeat delectus corrupti reprehenderit ea odio rerum est
+                harum laboriosam eligendi beatae iste minima dolores alias esse
+                amet officia. Cupiditate, mollitia! Porro vero asperiores
+                sapiente assumenda enim numquam? Nisi fugit sit iure placeat nam
+                corrupti, neque consequuntur et unde quibusdam aperiam soluta ea
+                libero, illum minima molestias exercitationem debitis explicabo
+                odit dolor laboriosam! Repellat reprehenderit minima repudiandae
+                doloremque eaque quidem optio deserunt, neque dolorum natus
+                magnam error maxime ab eos quas facilis perferendis eius nobis
+                rerum aperiam dolorem adipisci dolore in. Modi minima nam quod
+                debitis consequuntur ea, facere totam harum aperiam quisquam
+                obcaecati in numquam, officiis rem, pariatur iste eum sapiente
+                quam non placeat dolore libero? Laudantium dolorum deleniti sunt
+                necessitatibus incidunt eveniet iste blanditiis, commodi dolorem
+                vero nobis voluptas, accusamus atque quas exercitationem in
+                cumque maxime.
+            </p>
+        </div>
+    );
+};
+
+export default About;
